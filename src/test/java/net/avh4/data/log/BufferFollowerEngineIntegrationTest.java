b5a6d3876b7f3e6ddddc6f2db738ed2dd53d123b
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-12-23 23:09:56 -0800

    Clean up BufferFollowerEngineIntegrationTest

--- src/test/java/net/avh4/data/log/BufferFollowerEngineIntegrationTest.java
+++ src/test/java/net/avh4/data/log/BufferFollowerEngineIntegrationTest.java
@@ -10,16 +10,24 @@ public class BufferFollowerEngineIntegrationTest {
     private static final boolean DEBUG = false;
 
     private static class TestFollower implements TransactionFollower<TestFollower> {
-        public final HashMap<String, String> take_firsts;
-        public final HashMap<String, String> take_lasts;
+        private final HashMap<String, String> initialValues;
+        private final HashMap<String, String> mostRecentValues;
 
         public TestFollower() {
             this(new HashMap<String, String>(), new HashMap<String, String>());
         }
 
-        private TestFollower(HashMap<String, String> take_firsts, HashMap<String, String> take_lasts) {
-            this.take_firsts = take_firsts;
-            this.take_lasts = take_lasts;
+        private TestFollower(HashMap<String, String> initialValues, HashMap<String, String> mostRecentValues) {
+            this.initialValues = initialValues;
+            this.mostRecentValues = mostRecentValues;
+        }
+
+        public String initialValue(String id) {
+            return initialValues.get(id);
+        }
+
+        public String mostRecentValue(String id) {
+            return mostRecentValues.get(id);
         }
 
         @Override
@@ -28,22 +36,18 @@ public class BufferFollowerEngineIntegrationTest {
                 System.out.println(this + ": " + txn);
             }
             String[] keys = txn.key.split("/");
-            switch (keys[0]) {
-                case "take_first":
-                    if (take_firsts.containsKey(keys[1])) return;
-                    take_firsts.put(keys[1], txn.value);
-                    break;
-                case "take_last":
-                    take_lasts.put(keys[1], txn.value);
-                    break;
-                default:
-                    throw new RuntimeException("Unexpected key: " + txn);
+            if (keys.length == 2 && keys[1].equals("name")) {
+                if (!initialValues.containsKey(keys[0]))
+                    initialValues.put(keys[0], txn.value);
+                mostRecentValues.put(keys[0], txn.value);
+            } else {
+                throw new RuntimeException("Unexpected key: " + txn);
             }
         }
 
         @Override
         public TestFollower fork() {
-            TestFollower testFollower = new TestFollower(new HashMap<>(take_firsts), new HashMap<>(take_lasts));
+            TestFollower testFollower = new TestFollower(new HashMap<>(initialValues), new HashMap<>(mostRecentValues));
             if (DEBUG) {
                 System.out.println(testFollower + ": forked from " + this);
             }
@@ -54,27 +58,27 @@ public class BufferFollowerEngineIntegrationTest {
     @Test
     public void testBufferRollback() {
         TransientTransactionLog master = new TransientTransactionLog();
-        master.add("take_first/A", "master");
-        master.add("take_last/A", "master");
+        master.add("A/name", "master");
         TransactionBuffer buffer1 = new TransactionBuffer(master, master);
         TransactionBuffer buffer2 = new TransactionBuffer(master, master);
 
         BufferFollowerEngine<TestFollower> engine1 = new BufferFollowerEngine<>(buffer1, new TestFollower());
 
-        assertThat(engine1.result().take_firsts.get("A")).isEqualTo("master");
-        assertThat(engine1.result().take_lasts.get("A")).isEqualTo("master");
+        assertThat(engine1.result().initialValue("A")).isEqualTo("master");
+        assertThat(engine1.result().mostRecentValue("A")).isEqualTo("master");
 
-        buffer1.add("take_last/A", "client1");
-        assertThat(engine1.result().take_lasts.get("A")).isEqualTo("client1");
+        buffer1.add("A/name", "client1");
+        assertThat(engine1.result().mostRecentValue("A")).isEqualTo("client1");
 
         buffer1.flush();
-        assertThat(engine1.result().take_lasts.get("A")).isEqualTo("client1");
+        assertThat(engine1.result().mostRecentValue("A")).isEqualTo("client1");
 
-        buffer1.add("take_first/B", "client1");
-        buffer2.add("take_first/B", "client2");
-        assertThat(engine1.result().take_firsts.get("B")).isEqualTo("client1");
+        buffer1.add("B/name", "client1");
+        buffer2.add("B/name", "client2");
+        assertThat(engine1.result().initialValue("B")).isEqualTo("client1");
         buffer2.flush();
+        assertThat(engine1.result().initialValue("B")).isEqualTo("client2");
         buffer1.flush();
-        assertThat(engine1.result().take_firsts.get("B")).isEqualTo("client2");
+        assertThat(engine1.result().initialValue("B")).isEqualTo("client2");
     }
 }
